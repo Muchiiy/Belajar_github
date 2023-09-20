2905e1479c1147f8d2b2a1ca9a485a98691c8997
Muchiiy <145539504+Muchiiy@users.noreply.github.com>
2023-09-20 15:10:50 +0700

    Update Biodata.kt

--- Biodata.kt
+++ Biodata.kt
@@ -1,1 +1,10 @@
+fun main()
+{
+  var nama = "Mutiara Azizah"
+  var kelas = "XI RPL 2"
+  var hobi = "Menulis"
 
+  println("Nama : $nama")
+  println("Kelas : $kelas")
+  println("Hobi : $hobi")
+}
